78b74258d985e5085ab75c16a60cc7a205cd2381
navindu-sachintha <navindu.social@gmail.com>
2023-03-04 20:05:02 +0530

    Added program steps as comments

---
+++ project.js
@@ -0,0 +1,13 @@
+/*
+--------------- Program Steps ---------------------
+ 1. Deposite Money
+ 2. Determine the number of lines to bet on
+ 3. Collect bet amount
+ 4. Spin the slot machine
+ 5. Check if user won
+ 6. Give  the user  their winnigs or take the lost
+ 7. Play again
+ --------------------------------------------------
+*/
+
+
