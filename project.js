c8da696bf65608e0e8686cd45ee6e8d735df73c3
navindu-sachintha <navindu.social@gmail.com>
2023-03-07 23:34:02 +0530

    Created function to spin the slot machine

--- project.js
+++ project.js
@@ -69,7 +69,31 @@ const getBet = (balance, lines) => {
   }
 }
 
-let balance = deposite();
+const spin = () => {
+  const symbols = [];
+
+  for (const [symbol, count] of Object.entries(SYMBOL_COUNT)){
+    for(let i =0; i < count; i++){
+      symbols.push(symbol);
+    }
+  }
+  
+  const reels = [[],[],[]];
+
+  for (i = 0; i<COLS; i++){
+    const reelSymbols = [...symbols];
+
+    for(j=0; j<ROWS; j++){
+      const randomIndex = Math.floor(Math.random()*reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
+    }
+  }
+  return reels;
+};
+console.log(spin());
+/*let balance = deposite();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance,numberOfLines);
-console.log(bet);
+console.log(bet);*/
